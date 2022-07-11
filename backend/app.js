73a8da42bf63d4a588717c3cc47fc81b342d39cb
alamasyarie <alamasyarieee@gmail.com>
2022-07-11 10:38:34 +0700

    Init database server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// mongoose.connect('mongodb://localhost:27017/mycorner_db', { useNewParser: true, useUnifiedTopology: true })
+
+// const db = mongoose.conection
+
+mongoose.connect('mongodb://localhost:27017/myCorner_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Database conencted'))
 
 console.log('Hello world');
 
